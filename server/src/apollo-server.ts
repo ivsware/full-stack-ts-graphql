a10261e23a317e3aab2d664c8c9e67bf25c72530
Ivan Shastakou <ivan_shastakou2@epam.com>
2022-04-17 19:58:32 +0200

    apollo-server returns data

--- server/src/apollo-server.ts
+++ server/src/apollo-server.ts
@@ -4,34 +4,55 @@ import * as express from 'express';
 import { Server } from 'http';
 import Db from './db';
 
+const typeDefs = gql`
+  type Query {
+    currentUser: User!
+    suggestions: [Suggestion!]!
+  }
+  type User {
+    id: String!
+    name: String!
+    handle: String!
+    coverUrl: String!
+    avatarUrl: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Suggestion {
+    name: String!
+    handle: String!
+    avatarUrl: String!
+    reason: String!
+  }
+`;
+const resolvers = {
+  Query: {
+    currentUser: () => {
+      return {
+        id: '123',
+        name: 'John Doe',
+        handle: 'johndoe',
+        coverUrl: '',
+        avatarUrl: '',
+        createdAt: '',
+        updatedAt: '',
+      };
+    },
+    suggestions: () => {
+      return [];
+    },
+  },
+};
+
 export async function createApolloServer(
-  _db: Db,
+  db: Db,
   httpServer: Server,
   app: express.Application
 ): Promise<ApolloServer<ExpressContext>> {
-  const typeDefs = gql`
-    type Query {
-      currentUser: User!
-      suggestions: [Suggestion!]!
-    }
-    type User {
-      id: String!
-      name: String!
-      handle: String!
-      coverUrl: String!
-      avatarUrl: String!
-      createdAt: String!
-      updatedAt: String!
-    }
-    type Suggestion {
-      name: String!
-      handle: String!
-      avatarUrl: String!
-      reason: String!
-    }
-  `;
   const server = new ApolloServer({
     typeDefs,
+    resolvers,
+    context: () => ({ db }),
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
